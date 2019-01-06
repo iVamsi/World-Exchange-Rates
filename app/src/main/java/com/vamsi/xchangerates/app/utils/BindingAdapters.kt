0b0314d417a0a8d4ada42209304e338074239fe3
Vamsi <playstore.vamsi@gmail.com>
2019-01-06 15:57:20 -0500

    Added layout for currency converter fragment

--- app/src/main/java/com/vamsi/xchangerates/app/utils/BindingAdapters.kt
+++ app/src/main/java/com/vamsi/xchangerates/app/utils/BindingAdapters.kt
@@ -1,18 +1,145 @@
 package com.vamsi.xchangerates.app.utils
 
+import android.content.Context
+import android.os.Build
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.vamsi.xchangerates.app.R
 import java.util.*
 
 object BindingAdapters {
-    @JvmStatic
     @BindingAdapter("imageUrl")
-    fun bindImage(view: ImageView, fileName: String) {
+    @JvmStatic fun bindImage(view: ImageView, fileName: String) {
         val imageUrl = "flag_".plus(fileName.toLowerCase(Locale.US))
         Glide.with(view.context).load(view.context
             .resources
             .getIdentifier(imageUrl, "drawable", view.context.packageName))
             .into(view)
+    }
+
+    /**
+     * When defined in an [EditText], this [BindingAdapter] will clear the text on focus and
+     * set the previous value if the user doesn't enter one. When the focus leaves, it calls
+     * the listener that was passed as an argument.
+     *
+     * Note that `android:selectAllOnFocus="true"` does something similar but not exactly the same.
+     *
+     * @see [clearTextOnFocus] for a version without a listener.
+     */
+    @BindingAdapter("clearOnFocusAndDispatch")
+    @JvmStatic fun clearOnFocusAndDispatch(view: EditText, listener: View.OnFocusChangeListener?) {
+        view.onFocusChangeListener = View.OnFocusChangeListener { focusedView, hasFocus ->
+            val textView = focusedView as TextView
+            if (hasFocus) {
+                // Delete contents of the EditText if the focus entered.
+                view.setTag(R.id.previous_value, textView.text)
+                textView.text = ""
+            } else {
+                if (textView.text.isEmpty()) {
+                    val tag: CharSequence? = textView.getTag(R.id.previous_value) as CharSequence
+                    textView.text = tag ?: ""
+                }
+                // If the focus left, update the listener
+                listener?.onFocusChange(focusedView, hasFocus)
+            }
+        }
+    }
+
+    /**
+     * Clears the text on focus.
+     *
+     * This method is using extension functions. It's equivalent to:
+     * ```
+     * @JvmStatic fun clearTextOnFocus(view: EditText, enabled: Boolean)...
+     * ```
+     */
+    @BindingAdapter("clearTextOnFocus")
+    @JvmStatic fun EditText.clearTextOnFocus(enabled: Boolean) {
+        if (enabled) {
+            clearOnFocusAndDispatch(this, null)
+        } else {
+            this.onFocusChangeListener = null
+        }
+    }
+
+    /**
+     * Hides keyboard when the [EditText] is focused.
+     *
+     * Note that there can only be one [TextView.OnEditorActionListener] on each [EditText] and
+     * this [BindingAdapter] sets it.
+     */
+    @BindingAdapter("hideKeyboardOnInputDone")
+    @JvmStatic fun hideKeyboardOnInputDone(view: EditText, enabled: Boolean) {
+        if (!enabled) return
+        val listener = TextView.OnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                view.clearFocus()
+                val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE)
+                        as InputMethodManager
+                imm.hideSoftInputFromWindow(view.windowToken, 0)
+            }
+            false
+        }
+        view.setOnEditorActionListener(listener)
+    }
+
+    @BindingAdapter("loseFocusWhen")
+    @JvmStatic fun loseFocusWhen(view: EditText, condition: Boolean) {
+        if (condition) view.clearFocus()
+    }
+
+    /*
+     * Instead of having if-else statements in the XML layout, you can create your own binding
+     * adapters, making the layout easier to read.
+     *
+     * Instead of
+     *
+     * `android:visibility="@{viewmodel.isStopped ? View.INVISIBLE : View.VISIBLE}"`
+     *
+     * you use:
+     *
+     * `android:invisibleUnless="@{viewmodel.isStopped}"`
+     *
+     */
+
+    /**
+     * Makes the View [View.INVISIBLE] unless the condition is met.
+     */
+    @Suppress("unused")
+    @BindingAdapter("invisibleUnless")
+    @JvmStatic fun invisibleUnless(view: View, visible: Boolean) {
+        view.visibility = if (visible) View.VISIBLE else View.INVISIBLE
+    }
+
+    /**
+     * Makes the View [View.GONE] unless the condition is met.
+     */
+    @Suppress("unused")
+    @BindingAdapter("goneUnless")
+    @JvmStatic fun goneUnless(view: View, visible: Boolean) {
+        view.visibility = if (visible) View.VISIBLE else View.GONE
+    }
+
+    /**
+     * In [ProgressBar], [ProgressBar.setMax] must be called before [ProgressBar.setProgress].
+     * By grouping both attributes in a BindingAdapter we can make sure the order is met.
+     *
+     * Also, this showcases how to deal with multiple API levels.
+     */
+    @BindingAdapter(value=["android:max", "android:progress"], requireAll = true)
+    @JvmStatic fun updateProgress(progressBar: ProgressBar, max: Int, progress: Int) {
+        progressBar.max = max
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            progressBar.setProgress(progress, false)
+        } else {
+            progressBar.progress = progress
+        }
     }
 }
