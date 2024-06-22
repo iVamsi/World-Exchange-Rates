485daf2d51b319ae750d718afbe8569af929f0f4
Vamsi Vaddavalli <playstore.vamsi@gmail.com>
2024-06-22 01:07:43 -0500

    Fix navigation

--- app/src/main/java/com/vamsi/xchangerates/app/view/ui/MainActivity.kt
+++ app/src/main/java/com/vamsi/xchangerates/app/view/ui/MainActivity.kt
@@ -3,28 +3,41 @@ package com.vamsi.xchangerates.app.view.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.vamsi.xchangerates.app.R
+import com.vamsi.xchangerates.app.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val navView: BottomNavigationView = binding.navigationView
+
+        // Find the NavHostFragment
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.navFragment) as NavHostFragment
+
+        // Obtain NavController from NavHostFragment
         val navController = navHostFragment.navController
 
-        setupActionBarWithNavController(navController)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.navFragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        return navController.navigateUp() || super.onSupportNavigateUp()
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_all_currencies, R.id.navigation_favorite_currencies, R.id.navigation_currency_converter
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
